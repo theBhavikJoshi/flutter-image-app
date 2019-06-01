b50377e937aa59afa62f844f385c0dc5e93b51a1
BhavikJoshi <bhavik@getwalnut.com>
2019-06-01 13:31:44 +0530

    Added Image and title Display

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -11,9 +11,30 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Text(images[index].url);
+        return buildImage(images[index]);
       },
     );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+        ),
+        padding: EdgeInsets.all(15.0),
+        margin: EdgeInsets.all(15.0),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              child: Image.network(image.url),
+              padding: EdgeInsets.only(
+                bottom: 10.0
+              ),
+            ),
+            Text(image.title)
+          ],
+        )
+      );
   }
 
 }
